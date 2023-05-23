3c37733c0179e1bb8178c087f9a60eb1d05a6f17
a.sharov <a.sharov@adriver.ru>
2023-05-23 14:22:14 +0300

    sample config added

---
+++ config.sample.py
@@ -0,0 +1,19 @@
+database = {
+    "host": "localhost",
+    "port": "3306",
+    "user": "library_user",
+    "password": "",
+    "database": ""
+}
+
+buttons_style = {
+    "in_table_buttons": {
+        "height": 16,
+        "width": 10
+    },
+    "other_buttons": {
+        "height": 16,
+        "width": 100
+    }
+
+}
